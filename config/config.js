84561ab4cf687f00bfe4a4329958bd2b6dcc96c1
Julia Lima <julia.manvailer17@icloud.com>
2022-12-14 17:44:08 -0500

    .

--- config/config.js
+++ config/config.js
@@ -10,7 +10,7 @@ module.exports = {
     dialect: 'postgres'
   },
   production: {
-    use_env_variable: 'DATABASE_URL',
+    use_env_variable: 'https://git.heroku.com',
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
