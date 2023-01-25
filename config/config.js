bf36bb1068b36fe96457124c908c0a61118ef156
Julia Lima <julia.manvailer17@icloud.com>
2023-01-25 11:32:18 -0600

    .

--- config/config.js
+++ config/config.js
@@ -3,14 +3,17 @@ require('dotenv').config()
 module.exports = {
   development: {
     database: 'weekendgetaway_development',
+    host: "127.0.0.1",
     dialect: 'postgres'
   },
   test: {
     database: 'weekendgetaway_test',
+    host: "127.0.0.1",
     dialect: 'postgres'
   },
   production: {
     use_env_variable: 'DATABASE_URL',
+    host: "127.0.0.1",
     dialect: 'postgres',
     protocol: "postgres",
     dialectOptions: {
